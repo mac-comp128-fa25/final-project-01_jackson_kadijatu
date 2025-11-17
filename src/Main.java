bbeb9a6f65d1ed2bd23aea7850280d1f4cea67e1
Jackson-Powers <jpowers@macalester.edu>
2025-11-17 14:08:59 -0600

    in prog

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
 /**
  * This is the main class for the game
  */
 
 public class Main {
+    private int numCustomers = 6;
+
+    private boolean[][] adjMatrix = new boolean[numCustomers][numCustomers];
+
+    // for(int i = 0; i< adjMatrix.length; i++ ){
+    //     for(int j =0; j< adjMatrix[i].length; j++){
+
+    //     }
+    // }
+
+
+    //Should be in its own class
+    public void makeInitGraph(){
+        adjMatrix[0][1] = true;
+    }
+
+
+
+
+
 }
