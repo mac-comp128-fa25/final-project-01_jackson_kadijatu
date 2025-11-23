5a758b0dcda70727c56cfeccc0eae72b4c08c3ee
Jackson-Powers <jpowers@macalester.edu>
2025-11-22 21:43:47 -0600

    Ingredient constructor finished
    Modified ingredients.txt structure and added a precondition for formatting

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -6,11 +7,38 @@ import java.util.HashMap;
 
 public class Ingredient {
     
-    HashMap<Ingredient, String> Definitions = new HashMap<>();
+    HashMap<String, ArrayList<String>> Definitions = new HashMap<>();
 
 
-    public Ingredient(){
-        
+    //Precondition of file structure: Last attibute for a given ingredient precedes a period.
+    public Ingredient(String fileName){
+
+        String ingredient = "";
+        ArrayList<String> attributes = new ArrayList<>();
+        try {
+            java.io.File file = new java.io.File("src/ingredients/" + fileName);
+            java.util.Scanner input = new java.util.Scanner(file);
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                if (line.contains("-")) {
+                    attributes.add(line);
+                }
+                else{
+                    ingredient = line;
+                }
+
+                if(line.contains(".")){
+                    Definitions.put(ingredient, attributes);
+                    attributes = new ArrayList<>();
+                }
+                
+            }
+            input.close();
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+
+
     }
     
 }
