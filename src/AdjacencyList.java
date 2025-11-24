c2bbb94d58b79970335eb66934daf3a133eec8db
Jackson-Powers <jpowers@macalester.edu>
2025-11-24 14:10:42 -0600

    in prog

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -50,37 +50,12 @@ private HashMap<Customer, ArrayList<Customer>> customerConnections;
 
 
     public void initialSetup(){
-    //    Customer A = new Customer("adrien.txt");
-    //    Customer B = new Customer("ava.txt");
-    //    Customer C = new Customer("Dennis.txt");
-    //    Customer D = new Customer("holly.txt");
-    //    Customer E = new Customer("nova.txt");
-    //    Customer F = new Customer("shawn.txt");
-
 
        ArrayList<Customer> customers = Main.populateAllCustomers("src/Customers");
 
        for(Customer c : customers){
         customerConnections.put(c, c.connections);
        }
-
-       //java file interation instead of hardcode
-
-
-
-       //Use the getConnections method once it is made instead of hardcoding the neighbors list
-
-
-
-        //change to a loop when change file so this isnt hard coded
-
-        // customerConnections.put(A, A.connections);
-        // customerConnections.put(B, B.connections);
-        // customerConnections.put(C, C.connections);
-        // customerConnections.put(D, D.connections);
-        // customerConnections.put(E, E.connections);
-        // customerConnections.put(F, F.connections);
-       
 
 
     }
