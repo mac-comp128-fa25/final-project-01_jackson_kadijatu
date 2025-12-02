71c33292a878b3732984e43b9cee7b88ff6d7fbb
Jackson-Powers <jpowers@macalester.edu>
2025-12-02 16:02:21 -0600

    Moving around customer connections method to adjList

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 public class AdjacencyList {
 
 private HashMap<Customer, ArrayList<Customer>> customerConnections;
+private ArrayList<Customer> connections; 
 
 
 
     public AdjacencyList(){
         customerConnections = new HashMap<>();
+        connections = new ArrayList<>();
         
     }
 
@@ -60,5 +62,43 @@ private HashMap<Customer, ArrayList<Customer>> customerConnections;
 
 
 
-    
+
+
+    public ArrayList<Customer> getConnections(String fileName) {
+        ArrayList<Customer> connections = new ArrayList<Customer>();
+
+        try {
+            java.io.File file = new java.io.File("res/allTextFiles/Customers/" + fileName);
+            java.util.Scanner input = new java.util.Scanner(file);
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+
+                if (line.startsWith("Connections:")) {
+                    String[] names = line.substring(12).trim().split(" ");
+
+                    for (String name : names) {
+                        Customer c = new Customer(name + ".txt");   
+                        connections.add(c);
+                    
+                    }
+            }
+        }
+        input.close();
+
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+        this.connections = connections;
+        return connections;
+    }
+
+
+
+    public ArrayList<Customer> getConnections(){
+        return connections;
+    }
 }
+
+    
+
